b44c55da44b6368d43e1225cb8753402bf06548e
dolphinspired <runeberry.software@gmail.com>
2016-11-25 22:16:24 -0500

    Adding simple unit test

---
+++ Wox.Plugin.OldSchoolRunescape.Tests/PluginTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Wox.Plugin.OldSchoolRunescape.Tests
+{
+    [TestFixture]
+    public class PluginTests
+    {
+        private Main _main;
+
+        [TestCase("runite ore")]
+        public void TestPlugin(string search)
+        {
+            var query = new Query
+            {
+                ActionKeyword = "osrs",
+                Terms = search.Split(' ')
+            };
+
+            if (_main == null)
+            {
+                _main = new Main();
+            }
+
+            List<Result> results = _main.Query(query);
+
+            Assert.That(results, Is.Not.Null);
+        }
+    }
+}
