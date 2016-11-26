f14eb259ee868611e0f5246c79dfd10309a803d3
dolphinspired <runeberry.software@gmail.com>
2016-11-25 20:24:42 -0500

    Adding Json.Net and response models

---
+++ Wox.Plugin.OldSchoolRunescape/Models/WikiaSearchItem.cs
@@ -0,0 +1,17 @@
+﻿namespace Wox.Plugin.OldSchoolRunescape.Models
+{
+    public class WikiaSearchItem
+    {
+        public int Quality { get; set; }
+
+        public string Url { get; set; }
+
+        public int Ns { get; set; }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Snippet { get; set; }
+    }
+}
---
+++ Wox.Plugin.OldSchoolRunescape/Models/WikiaSearchResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Wox.Plugin.OldSchoolRunescape.Models
+{
+    public class WikiaSearchResponse
+    {
+        public int Batches { get; set; }
+
+        public int Total { get; set; }
+
+        public int CurrentBatch { get; set; }
+
+        public int Next { get; set; }
+
+        public List<WikiaSearchItem> Items { get; set; }
+    }
+}
